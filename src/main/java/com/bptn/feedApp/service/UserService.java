0ae9e3d0f0fc3b46e1d2d9e85213a0dd7e2f8a7c
Chukwuebuka Madu <louisfrancismaduc@gmail.com>
2024-11-04 16:48:18 -0500

    UserService

---
+++ src/main/java/com/bptn/feedApp/service/UserService.java
@@ -0,0 +1,28 @@
+package com.bptn.feedApp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bptn.feedApp.jdbc.UserBean;
+import com.bptn.feedApp.jdbc.UserDao;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserDao userDao;
+
+	public List<UserBean> listUsers() {
+		return this.userDao.listUsers();
+	}
+
+	public UserBean findByUsername(String username) {
+		return this.userDao.findByUsername(username);
+	}
+
+	public void createUser(UserBean user) {
+		this.userDao.createUser(user);
+	}
+}
