5340340ec51e6d7e88c30a66859255ceaa7bf6c6
Chukwuebuka Madu <louisfrancismaduc@gmail.com>
2024-11-08 11:47:27 -0500

    update application

--- src/main/java/com/bptn/feedApp/service/UserService.java
+++ src/main/java/com/bptn/feedApp/service/UserService.java
@@ -20,6 +20,9 @@ public class UserService {
 	@Autowired
 	UserRepository userRepository;
 
+	@Autowired
+	EmailService emailService;
+
 	public List<User> listUsers() {
 		return this.userRepository.findAll();
 	}
@@ -41,6 +44,8 @@ public class UserService {
 
 		user.setCreatedOn(Timestamp.from(Instant.now()));
 		this.userRepository.save(user);
+
+		this.emailService.sendVerificationEmail(user);
 		return user;
 	}
 }
