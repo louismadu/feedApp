01639178dd2341206e6b6504dfc7087ea43a7ca1
Chukwuebuka Madu <louisfrancismaduc@gmail.com>
2024-11-08 13:55:24 -0500

    Exceptions

---
+++ src/main/java/com/bptn/feedApp/exception/domain/EmailExistException.java
@@ -0,0 +1,12 @@
+package com.bptn.feedApp.exception.domain;
+
+public class EmailExistException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public EmailExistException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
