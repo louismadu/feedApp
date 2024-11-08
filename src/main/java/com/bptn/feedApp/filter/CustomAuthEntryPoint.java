aa1acabe9a35588431ec54ee875e6fbe2f49a2e2
Chukwuebuka Madu <louisfrancismaduc@gmail.com>
2024-11-08 14:35:52 -0500

    AuthenticationEntryPoint

---
+++ src/main/java/com/bptn/feedApp/filter/CustomAuthEntryPoint.java
@@ -0,0 +1,28 @@
+package com.bptn.feedApp.filter;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomAuthEntryPoint implements AuthenticationEntryPoint {
+
+	@Autowired
+	@Qualifier("handlerExceptionResolver")
+	HandlerExceptionResolver resolver;
+
+	@Override
+	public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException ex)
+			throws IOException {
+		// Resolve the exception and return an appropriate response
+		this.resolver.resolveException(req, res, null, ex);
+	}
+}
