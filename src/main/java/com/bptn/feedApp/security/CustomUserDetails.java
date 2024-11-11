2bddd1d975005a15e628c32353cb5a2fa352ea87
Chukwuebuka Madu <louisfrancismaduc@gmail.com>
2024-11-11 14:20:31 -0500

    userService

--- src/main/java/com/bptn/feedApp/security/CustomUserDetails.java
+++ src/main/java/com/bptn/feedApp/security/CustomUserDetails.java
@@ -1,6 +1,7 @@
 package com.bptn.feedApp.security;
 
 import java.util.Collection;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -8,55 +9,55 @@ import com.bptn.feedApp.jpa.User;
 
 public class CustomUserDetails implements UserDetails {
 
- private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
- @Override
- public Collection<? extends GrantedAuthority> getAuthorities() {
-     
-     return null;
- }
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
 
- @Override
- public String getPassword() {
-     
-     return null;
- }
+		return null;
+	}
 
- @Override
- public String getUsername() {
-     
-     return null;
- }
+	@Override
+	public String getPassword() {
 
- @Override
- public boolean isAccountNonExpired() {
-     
-     return false;
- }
+		return this.user.getPassword();
+	}
 
- @Override
- public boolean isAccountNonLocked() {
-     
-     return false;
- }
+	@Override
+	public String getUsername() {
 
- @Override
- public boolean isCredentialsNonExpired() {
-     
-     return false;
- }
+		return this.user.getUsername();
+	}
 
- @Override
- public boolean isEnabled() {
-     
-     return false;
- }
- 
- User user;
+	@Override
+	public boolean isAccountNonExpired() {
 
-public CustomUserDetails(User user) {
-	super();
-	this.user = user;
-}
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+
+		return true;
+	}
+
+	User user;
+
+	public CustomUserDetails(User user) {
+		super();
+		this.user = user;
+	}
 
 }
