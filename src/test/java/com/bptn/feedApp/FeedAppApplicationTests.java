b17169e5f514c6016c29fc24e6ba716cef3c3ae8
Chukwuebuka Madu <louisfrancismaduc@gmail.com>
2024-11-14 13:29:25 -0500

    FeedAppApplicationTests

--- src/test/java/com/bptn/feedApp/FeedAppApplicationTests.java
+++ src/test/java/com/bptn/feedApp/FeedAppApplicationTests.java
@@ -1,13 +1,22 @@
 package com.bptn.feedApp;
 
+import org.h2.tools.Server;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FeedAppApplicationTests {
 
+	@DisplayName("Demo Test")
 	@Test
 	void contextLoads() {
 	}
+
+	@MockBean
+	Server server;
 
 }
